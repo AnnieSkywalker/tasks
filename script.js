2a33c2b2cb1074bdaa14ee5b4e0146bf37bf7118
spiridonov <annie.skywalker@yandex.ru>
2025-01-01 16:53:05 +0300

    счётчик времени последнего вызова функции

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ const array = [5, 2, 100, 6, 10, 70];
 const arrayRev = [];
 
 for (let i = array.length; i > 0; i--) {
-     arrayRev.push(array[i-1])   
+     arrayRev.push(array[i-1]);
 }
 
-console.log(arrayRev)
+// console.log(arrayRev)
+
+//когда последний раз вызывалась функция
+function closing() {
+    let time = 0;
+    
+    return function counter() {
+        alert(time)
+        time = Date.now();
+    }
+}
+
+let cl = closing();
+
+cl();
+cl();
