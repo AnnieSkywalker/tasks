c20e663323a2744d8cd1c1fdee6434d21aae506c
spiridonov <annie.skywalker@yandex.ru>
2025-01-01 16:07:42 +0300

    task revert array

---
+++ script.js
@@ -0,0 +1,10 @@
+//array revert
+const array = [5, 2, 100, 6, 10, 70];
+
+const arrayRev = [];
+
+for (let i = array.length; i > 0; i--) {
+     arrayRev.push(array[i-1])   
+}
+
+console.log(arrayRev)
